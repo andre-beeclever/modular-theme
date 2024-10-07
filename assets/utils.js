757ab2bfab16935a6fc9020bfcf1847019e2e2a0
André Neder <andre@beeclever.de>
2024-10-07 17:05:59 +0200

    fix

--- assets/utils.js
+++
@@ -1,2 +0,0 @@
-Object.defineProperty(Array.prototype,"uniq",{value:function(){return Array.from(new Set(this))}}),Object.defineProperty(HTMLElement.prototype,"hide",{value:function(){this.setAttribute("hidden","hidden")}}),Object.defineProperty(HTMLElement.prototype,"show",{value:function(){this.removeAttribute("hidden")}});
-//# sourceMappingURL=utils.js.map
