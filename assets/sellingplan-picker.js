19a2c56741a4285378dbe18a2653eaca9061baea
Benedikt Krieger <benedikt@beeclever.de>
2024-10-10 18:35:32 +0200

    product add to cart button

--- assets/sellingplan-picker.js
+++ assets/sellingplan-picker.js
@@ -5,17 +5,26 @@ class SellingPlanPicker extends HTMLElement {
   connectedCallback(){
     this.sellinplanInput = this.querySelector('input[name="selling_plan"]');
     const purchseOptions = this.querySelectorAll('.purchase-option');
+    const sellingplanOptions = this.querySelectorAll('.sellingplan-input');
     for(const purchseOption of purchseOptions) {
       const input = purchseOption.querySelector('input[name="purchase-option"]');
       const content = purchseOption.querySelector('.purchase-option-content');
       input.addEventListener('input', () => {
         if(!!content) {
-          let selected_plan = content.querySelector('input:checked') || content.querySelector('input');
+          let selected_plan = content.querySelector('input.sellingplan-input:checked') || content.querySelector('input.sellingplan-input');
           if(!!selected_plan) {
+            console.log('set purchase option selected plan: '+selected_plan.value);
             selected_plan.checked = true;
             selected_plan.dispatchEvent(new Event('input'));
           }
         }
+      });
+    }
+    for(const sellingplanOption of sellingplanOptions) {
+      sellingplanOption.addEventListener('input', () => {
+        this.sellinplanInput.value = sellingplanOption.value;
+        console.log('set selling plan main input to: '+sellingplanOption.value);
+        //this.sellinplanInput.dispatchEvent(new Event('input'));
       });
     }
   }
